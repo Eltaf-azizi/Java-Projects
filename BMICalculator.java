4e94a129318e1ee402bbab5036b63077cf3b6e07
unknown <iltfhuss@gmail.com>
2024-05-14 21:12:48 +0500

    added soduko solver

--- BMICalculator.java
+++ BMICalculator.java
@@ -1,18 +1,26 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class BMICalculator {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter weight in pounds:");
         double weight = input.nextDouble();
 
+        System.out.println("Enter height in feet:");
+        int heightFeet = input.nextInt();
+
         System.out.println("Enter height in inches");
-        double height = input.nextDouble();
+        int heightInches = input.nextInt();
 
-        System.out.println(weight);
-        System.out.println(height);
+        int totalHeightInInches = (heightFeet * 12) + heightInches;
+        input.close();
 
-        double bmi = weight / Math.pow(height, 2) * 703;
-        System.out.println("Your BMI is: " + bmi);
+        double bmi = weight / Math.pow(totalHeightInInches, 2) * 703;
+
+        DecimalFormat df = new DecimalFormat("0.0");
+        System.out.println("Your BMI is: " + df.format(bmi));
     }
 }
