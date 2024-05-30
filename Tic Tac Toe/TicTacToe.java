a0848596a357fd91bb5ed61782bc9a6fbfb8e663
unknown <iltfhuss@gmail.com>
2024-05-30 22:44:03 +0500

    tic tac toe

---
+++ Tic Tac Toe/TicTacToe.java
@@ -0,0 +1,23 @@
+import java.awt.event.*;
+
+
+
+public class TicTacToe implements ActionListener{
+        TicTacToe(){
+        }  
+        public void actionPerformed(ActionEvent e) {
+
+        }
+        public void firstTurn() {
+
+        }
+        public void check(){
+
+        }
+        public void xWins(int a, int b, int c) {
+
+        }
+        public void oWins(int a, int b, int c) {
+
+        }
+    }
