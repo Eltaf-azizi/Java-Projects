d736f3a6476f2aacdd59b3ed7bca880558456a2d
unknown <iltfhuss@gmail.com>
2024-06-06 23:47:57 +0500

    added Tic Tac Toe game

--- Tic Tac Toe/TicTacToe.java
+++ Tic Tac Toe/TicTacToe.java
@@ -103,11 +103,158 @@ public class TicTacToe implements ActionListener{
 
     public void check(){
 
+        if(
+            (buttons[0].getText()=="X") &&
+            (buttons[1].getText()=="X") &&
+            (buttons[2].getText()=="X")
+        ){
+            xWins(0, 1, 2);
+        }
+
+        if(
+            (buttons[3].getText()=="X") &&
+            (buttons[4].getText()=="X") &&
+            (buttons[5].getText()=="X")
+        ){
+            xWins(3, 4, 5);
+        }
+        
+        if(
+            (buttons[6].getText()=="X") &&
+            (buttons[7].getText()=="X") &&
+            (buttons[8].getText()=="X")
+        ){
+            xWins(6, 7, 8);
+        }
+
+        if(
+            (buttons[0].getText()=="X") &&
+            (buttons[3].getText()=="X") &&
+            (buttons[6].getText()=="X")
+        ){
+            xWins(1, 3, 6);
+        }
+
+        if(
+            (buttons[1].getText()=="X") &&
+            (buttons[4].getText()=="X") &&
+            (buttons[7].getText()=="X")
+        ){
+            xWins(1, 4, 7);
+        }
+
+        if(
+            (buttons[2].getText()=="X") &&
+            (buttons[5].getText()=="X") &&
+            (buttons[8].getText()=="X")
+        ){
+            xWins(2, 5, 8);
+        }
+
+        if(
+            (buttons[0].getText()=="X") &&
+            (buttons[4].getText()=="X") &&
+            (buttons[8].getText()=="X")
+        ){
+            xWins(0, 4, 8);
+        }
+
+        if(
+            (buttons[2].getText()=="X") &&
+            (buttons[4].getText()=="X") &&
+            (buttons[6].getText()=="X")
+        ){
+            xWins(2, 4, 6);
+        }
+
+
+
+
+        
+
+
+        if(
+            (buttons[0].getText()=="O") &&
+            (buttons[1].getText()=="O") &&
+            (buttons[2].getText()=="O")
+        ){
+            oWins(0, 1, 2);
+        }
+
+        if(
+            (buttons[3].getText()=="O") &&
+            (buttons[4].getText()=="O") &&
+            (buttons[5].getText()=="O")
+        ){
+            oWins(3, 4, 5);
+        }
+        
+        if(
+            (buttons[6].getText()=="O") &&
+            (buttons[7].getText()=="O") &&
+            (buttons[8].getText()=="O")
+        ){
+            oWins(6, 7, 8);
+        }
+
+        if(
+            (buttons[0].getText()=="O") &&
+            (buttons[3].getText()=="O") &&
+            (buttons[6].getText()=="O")
+        ){
+            oWins(1, 3, 6);
+        }
+
+        if(
+            (buttons[1].getText()=="O") &&
+            (buttons[4].getText()=="O") &&
+            (buttons[7].getText()=="O")
+        ){
+            oWins(1, 4, 7);
+        }
+
+        if(
+            (buttons[2].getText()=="O") &&
+            (buttons[5].getText()=="O") &&
+            (buttons[8].getText()=="O")
+        ){
+            oWins(2, 5, 8);
+        }
+
+        if(
+            (buttons[0].getText()=="O") &&
+            (buttons[4].getText()=="O") &&
+            (buttons[8].getText()=="O")
+        ){
+            oWins(0, 4, 8);
+        }
+
+        if(
+            (buttons[2].getText()=="O") &&
+            (buttons[4].getText()=="O") &&
+            (buttons[6].getText()=="O")
+        ){
+            oWins(2, 4, 6);
+        }
     }
     public void xWins(int a, int b, int c) {
+        buttons[a].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
 
+        for(int i=0;i<9;i++) {
+            buttons[i].setEnabled(false);
+        }
+        textfield.setText("X wins");
     }
     public void oWins(int a, int b, int c) {
+        buttons[a].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
 
+        for(int i=0;i<9;i++) {
+            buttons[i].setEnabled(false);
+        }
+        textfield.setText("O wins");
     }
 }
